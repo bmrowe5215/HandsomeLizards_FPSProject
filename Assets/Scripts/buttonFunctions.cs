cb1fc55c855076774d6cd2db43bc0d0a8463689f
HKPepsi <76918271+HKPepsi@users.noreply.github.com>
2022-10-06 15:50:52 -0400

    Added 2 new menues: Victory and Death Menus, as well as funcionality/baseline framework for the Game Manager to handle calls from "Karen" (essentially anything that isn't the GameManager is a karen and makes calls) and to push and pull needed data.

--- Assets/Scripts/buttonFunctions.cs
+++ Assets/Scripts/buttonFunctions.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class buttonFunctions : MonoBehaviour
 {
-   //this is gonna call back to the game manager, say "hey, game's paused. let these buttons do things to unpause and or quit the game"
+    //this is gonna call back to the game manager, say "hey, game's paused. let these buttons do things to unpause and or quit the game"
     public void resume()
     {
         gameManager.instance.unpause();
@@ -20,5 +20,11 @@ public class buttonFunctions : MonoBehaviour
     public void quit()
     {
         Application.Quit();
+    }
+
+    public void respawn()
+    {
+        //gameManager.instance.playerScript.whatever because its not done yet.
+        //gameManager.instance.unpause(); karen asks the manager to unpause her game.
     }
 }
--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,14 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class gameManager : MonoBehaviour
 {
     //create the first instance of the game manager object.
+    //so when we need to access anything, "Karen" asks for the manager to handle certain tasks.
+    //For example, lets say our karen was found dead, she in spirit would ask for a manager to try and resurrect her.
+    //the manager pulls from the playerController script, disables it, moves the player (karen) object to the spawn position, then re-enables control.
+    //thus effectively "ressurecting" our "Karen".
     public static gameManager instance;
+    public GameObject player;
+
+    //To be added when we merge player scripts, but this'll just be here commented as placeholder.
+    //public playerController playerScript;
+
 
     //UI HANDLER STUFF
+    [Header("===== UI SCREENS =====")]
+    //karen wants a time out
     public GameObject pauseScreen;
+    //karen is dead
+    public GameObject deathScreen;
+    //karen succeeds in her penultimate task of being cringe.
+    public GameObject victoryScreen;
+    //karen asks for a manager to check if a menu is open before opening up another dialogue box.
+    public GameObject menuOpen;
+
+    [Header("===== Player HUD =====")]
+    //karen asks for how much health a player has, but needs it represented as a png.
+    public Image playerHPBar;
+    //karen asks for how many enemies/cashiers to harass to achieve her goal.
+    public TextMeshProUGUI enemyCounter;
+    //karen asks for how much health she has numerically, because pngs can be unreliable.
+    public TextMeshProUGUI hpNumber;
+
+    //karen timed out.
     public bool paused;
 
 
@@ -18,6 +48,10 @@ public class gameManager : MonoBehaviour
     {
         //initialize the instance
         instance = this;
+        //karen wants to be acknowledge as a player, (this is for the actual like player OBJECT not the SCRIPT) 
+        player = GameObject.FindGameObjectWithTag("Player");
+        //Karen wants to get to access to everything attached to the player controller, including functions n whatnot.
+        //playerScript = player.getComponent<playerController>();
         //now we're gaming, and can make calls n stuff.
         //This is where the player stuff would go, but i'm making the game manager *before* we even have a player object.
     }
@@ -55,4 +89,5 @@ public class gameManager : MonoBehaviour
         Cursor.visible = paused;
         Cursor.lockState = CursorLockMode.Confined;
     }
+
 }
